bed944578e9817e5fdbfcd329510986f2488cc51
TwitOrel <oreltwito3@gmail.com>
2025-04-07 12:39:37 +0200

    deploy.sh for the clound working well!

--- applifireProject/settings.py
+++ applifireProject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 from datetime import timedelta
 
 load_dotenv()
-print("check for deploy")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
